bbb6dec5dcd66b866d9f493aed6213e561e8fc10
KolesnichenkoAG <kag.1981@mail.ru>
2022-04-30 16:31:12 +0600

    исправил ошибку

--- src/homeWork/Lesson5.java
+++ src/homeWork/Lesson5.java
@@ -1,34 +1,36 @@
 package homeWork;
 
+import java.util.Arrays;
+
 public class Lesson5 {
     static final int SIZE = 10_000_000;
     static final int HALF = SIZE / 2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         firstMethod();
         secondMethod();
+
     }
 
     public static void firstMethod() {
         float[] arr1 = new float[SIZE];
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = 1.0f;
-        }
+        Arrays.fill(arr1, 1.0f);
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i
-                    / 5) * Math.cos(0.4f + i / 2));
+            arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5.0) * Math.cos(0.2f + i / 5.0) * Math.cos(0.4f + i / 2.0));
             //System.out.println("arr1 [" + i + "] =" + arr1[i]);
         }
         System.out.println("One thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 
-    public static void secondMethod() {
+    public static void secondMethod() throws InterruptedException {
         float[] arr2 = new float[SIZE];
         for (int i = 0; i < SIZE; i++) {
             arr2[i] = 1;
+            //System.out.println("arr2 [" + i + "] =" + arr2[i]);
         }
         long startTime = System.currentTimeMillis();
+
         float[] leftHalf = new float[HALF];
         float[] rightHalf = new float[HALF];
         System.arraycopy(arr2, 0, leftHalf, 0, HALF);
@@ -37,8 +39,8 @@ public class Lesson5 {
             @Override
             public void run() {
                 for (int i = 0; i < leftHalf.length; i++) {
-                    leftHalf[i] = (float) (leftHalf[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i
-                            / 5) * Math.cos(0.4f + i / 2));
+                    leftHalf[i] = (float) (leftHalf[i] * Math.sin(0.2f + i / 5.0) * Math.cos(0.2f + i / 5.0) * Math.cos(0.4f + i / 2.0));
+                    //System.out.println("leftHalf [" + i + "] =" + leftHalf[i]);
                 }
             }
         });
@@ -47,14 +49,17 @@ public class Lesson5 {
             @Override
             public void run() {
                 for (int i = 0; i < rightHalf.length; i++) {
-                    rightHalf[i] = (float) (rightHalf[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i
-                            / 5) * Math.cos(0.4f + i / 2));
+                    rightHalf[i] = (float) (rightHalf[i] * Math.sin(0.2f + i / 5.0) * Math.cos(0.2f + i / 5.0) * Math.cos(0.4f + i / 2.0));
                     //System.out.println("rigtHalf [" + i + "] =" + rightHalf[i]);
                 }
             }
         });
+
         thread1.start();
         thread2.start();
+        thread1.join();
+        thread2.join();
+
         float[] mergedArray = new float[SIZE];
         System.arraycopy(leftHalf, 0, mergedArray, 0, HALF);
         System.arraycopy(rightHalf, 0, mergedArray, HALF, HALF);
